55b2b7786faad6e672758768be1782ef38ac72ab
vladovelikov <vladimir.velikov1995@gmail.com>
2022-10-04 20:53:42 +0300

    Initial commit

--- user-service/src/main/java/com/microservices/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/microservices/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.microservices.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -9,5 +10,6 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+
 
 }
