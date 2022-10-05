5efe633a8a22c2f19e703c489173006389a19a5a
vladovelikov <vladimir.velikov1995@gmail.com>
2022-10-05 21:15:12 +0300

    Config update

--- user-service/src/main/java/com/microservices/userservice/config/ApplicationBeanConfiguration.java
+++ user-service/src/main/java/com/microservices/userservice/config/ApplicationBeanConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationBeanConfiguration {
 
     @Bean
-    private RestTemplate restTemplate() {
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
